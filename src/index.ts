9a40997c5c7867f3112298bddaecb99fedae36d0
adrianbalbs <adrianbalbs@hotmail.com>
2023-05-17 22:30:20 +1000

    Added discord id parameter to database

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,12 @@ const PORT = process.env.PORT;
 const prisma = new PrismaClient();
 const app = express();
 
+
+// Once I'm writing my discord bot, maybe split this up? 
 app.get('/callback', async (req: Request, res: Response) => {
-  const { code } = req.query; 
+  const { code, state } = req.query; 
   const basic = Buffer.from(process.env.SPOTIFY_CLIENT_ID + ":" + process.env.SPOTIFY_CLIENT_SECRET).toString('base64');
-  if (code) {
+  if (code && state) {
     try {
       const formData = new url.URLSearchParams({
         grant_type: 'authorization_code',
@@ -30,7 +32,13 @@ app.get('/callback', async (req: Request, res: Response) => {
           }
         }
       );
-      console.log(response.data);
+      
+      // Store authorized user into the database
+      console.log(response.data)
+      const { access_token, refresh_token } = response.data;
+      const user = await prisma.user.create({ data: { accessToken: access_token, refreshToken: refresh_token, discord_id: parseInt(state.toString()) }});
+      console.log(Number(user.discord_id));
+      res.sendStatus(200);
     } catch (err) {
       console.log(err);
       res.sendStatus(400);
