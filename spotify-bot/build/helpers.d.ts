dd38332fb5a36f66381fbd4d1127b6c6f6458663
Adrian Balbalosa <87012111+adrianbalbs@users.noreply.github.com>
2023-05-25 20:29:36 +1000

    Delete spotify-bot/build directory

--- spotify-bot/build/helpers.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function generateRandomString(length: number): string;
-export declare function wait(time: number): Promise<void>;
--- spotify-bot/build/spotify.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function getUserTokenAndStore(state: string, userId: string, user: string): Promise<boolean>;
-export declare function getDiscordUser(discordId: string): Promise<boolean>;
